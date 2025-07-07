dd0e044afdd880dc05780895b42edbc1337b863b
Nwabueze Ugoh <126014542+brentomagic@users.noreply.github.com>
2025-07-08 00:00:40 +0100

    Refactor GameCard and GameCardSkeleton to use GameCardContainer for consistent styling

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,7 +2,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card.Root borderRadius={10} overflow="hidden">
+    <Card.Root>
       {" "}
       {/* Removed fixed width */}
       <Skeleton height="200px" />
